ddc0cbdf4c9f9b67ef87b3e1eacdf36cd8a7c565
mt1006 <damianm916@gmail.com>
2022-06-15 00:33:54 +0200

    Add argParser

---
+++ cp/src/argParser.c
@@ -0,0 +1,14 @@
+#include "conplayer.h"
+
+/*typedef struct
+{
+	char* name;
+	char* fullName;
+	int (*parserFunction)(int, unichar);
+	int isOperation;
+} Option;
+
+void argumentParser()
+{
+
+}*/
